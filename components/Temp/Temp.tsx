58cb446e7ef0a3c437a475bb0e8a33bf5e6b866b
fam270589 <fam270589@gmail.com>
2022-12-07 17:24:05 +0700

    Add Temp

---
+++ components/Temp/Temp.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type Props = {
+  children?: React.ReactNode;
+  // props....
+};
+
+//todo:-----Temp component-----://
+const Temp = (props: Props) => {
+  return (
+    <div>Temp Component</div>
+  )
+}
+
+export default Temp;
